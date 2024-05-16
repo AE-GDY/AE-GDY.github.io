a3ffb534edd4c5c567346c190a6bb5257e7f0684
AE-GDY <74915033+AE-GDY@users.noreply.github.com>
2024-05-16 03:40:58 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d5e207b645af680c8e6289dd692888b7",
-"/": "d5e207b645af680c8e6289dd692888b7",
-"main.dart.js": "0062fc4ad8b8530c792d0d28841d3093",
+"index.html": "85e267d1cf241c938380784e013dd0bc",
+"/": "85e267d1cf241c938380784e013dd0bc",
+"main.dart.js": "5dd80224c73e45cf8345f7a83909638c",
 "manifest.json": "40ff209ae92f4a961f17fab38d932e01",
 "version.json": "2ba85ac1b0a2708dfc9ba6b4543299bf"};
 // The application shell files that are downloaded before a service worker can
